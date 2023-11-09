7117419ff24e307b5429254ee3875d5d275cc6da
Rina Iwamine <leddihome@gmail.com>
2023-11-10 00:41:40 +0900

    updated styles

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,9 +1,4 @@
 class BookmarksController < ApplicationController
-  def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
-  end
-
   def create
     @bookmark = Bookmark.new(bookmark_params)
 
@@ -12,7 +7,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@bookmark.list)
     else
-      render :new, status: :unprocesable_entity
+      render 'lists/show', status: :unprocesable_entity
     end
   end
 
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,6 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-  end
-
-  def new
     @list = List.new
   end
 
@@ -12,12 +9,13 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list)
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   private
