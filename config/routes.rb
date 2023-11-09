e416bf7d69bc4025ca8928387e600e90980b3625
Rina Iwamine <leddihome@gmail.com>
2023-11-09 11:14:38 +0900

    front end set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # As a user, I can see all my movie lists
+  # As a user, I can create a movie list
+  # As a user, I can see the details of a movie list
+  # As a user, I can bookmark a movie inside a movie list
+  # As a user, I can destroy a bookmark
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
