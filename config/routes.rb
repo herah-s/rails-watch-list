1ec88640c8a8cb7c11eae23b5021255c68ab1d2b
Rina Iwamine <leddihome@gmail.com>
2023-11-10 12:11:56 +0900

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # As a user, I can see the details of a movie list / GET "lists/42"
   # As a user, I can bookmark a movie inside a movie list / GET "lists/42/bookmarks/new" POST "lists/42/bookmarks"
   # As a user, I can destroy a bookmark / DELETE "bookmarks/25"
+  root to: 'lists#index'
+
   resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
