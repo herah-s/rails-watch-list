d21c5829dcf4dbabdd7a96bef00bf3ef875ccf34
Rina Iwamine <leddihome@gmail.com>
2023-11-10 15:18:23 +0900

    added active storage

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :image
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
+  validates :image, presence: true
 end
