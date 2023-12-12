68858a9461daa291ff3c34223ded5789e5b06eb0
Rina Iwamine <leddihome@gmail.com>
2023-12-12 16:26:26 +0900

    removed classname from bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   self.table_name = "watchlist_bookmarks"
-  belongs_to :movie, foreign_key: :watchlist_movie_id, class_name: "WatchlistMovie"
-  belongs_to :list, foreign_key: :watchlist_list_id, class_name: "WatchlistList"
+  belongs_to :movie, foreign_key: :watchlist_movie_id
+  belongs_to :list, foreign_key: :watchlist_list_id
 
   validates :comment, length: { minimum: 6 }
 
