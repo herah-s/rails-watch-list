3c07fc209c1170d2ffa4ff78ea4eb1e54b731304
Rina Iwamine <leddihome@gmail.com>
2023-12-12 16:22:39 +0900

    added classname to bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   self.table_name = "watchlist_bookmarks"
-  belongs_to :movie, foreign_key: :watchlist_movie_id
-  belongs_to :list, foreign_key: :watchlist_list_id
+  belongs_to :movie, class_name: "WatchlistMovie", foreign_key: :watchlist_movie_id
+  belongs_to :list, class_name: "WatchlistList", foreign_key: :watchlist_list_id
 
   validates :comment, length: { minimum: 6 }
 
